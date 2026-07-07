428b8cfb285d5a7d291967d99d90d51ad45ffbcd
supriyakumari20205-ctrl <supriyakumari20205@gmail.com>
2026-07-06 23:58:08 -0700

    Question-1.2.c
    Here It's the program to display the sum of two numbers

---
+++ Question-1.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    inta,b,sum;
+    printf("Enter the first number:");
+    scanf("%d,&a");
+    printf("Enter the second number:");
+    scanf("%d,&b");
+    Sum=a+b;
+    printf("Sum is:%d",sum);
+    return0;
+}
