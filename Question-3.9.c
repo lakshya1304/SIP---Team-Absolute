ac857a286e056d5d655794a3dfd8695131625123
supriyakumari20205-ctrl <supriyakumari20205@gmail.com>
2026-07-18 23:57:11 -0700

    Question-3.9
    Here is the C program to calculate the sum of digits.

---
+++ Question-3.9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    // input 123  --->  1+2+3=6
+    int n,sum=0,r;
+    printf("Enter any Number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+
+        r=n%10;
+        Sum=Sum+r;
+        n=n/10;
+    }
+    printf("Sum of digits: %d",sum);
+    return 0;
+}
+
