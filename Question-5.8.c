7bbbd36699c5954b9dc61b67894f1efe3a25e7ee
Supriya Kumari <supriyakumari20205@gmail.com>
2026-07-26 20:23:59 -0700

    Question-5.8
    Here is the C program to create function for palindrome.

---
+++ Question-5.8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int palindrome(int n)
+{
+    int temp, rev = 0, rem;
+    temp = n;
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+    if (temp == rev)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (palindrome(num))
+        printf("%d is a Palindrome number.\n", num);
+    else
+        printf("%d is not a Palindrome number.\n", num);
+
+    return 0;
+}
